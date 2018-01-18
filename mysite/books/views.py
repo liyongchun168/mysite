d698eafbeab800af5b4f7dcffb8b7459248bb3fe
liyongchun <liyongchun168@yeah.net>
2018-01-18 21:44:15 +0800

    20170118

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from mysite.books.models import Book
 
-def search_form(request):
-	return render_to_response('search_form.html')
+#def search_form(request):
+#	return render_to_response('search_form.html')
 
 def search(request):
-    if 'q' in request.GET and request.GET['q']:
+    errors = []
+    if 'q' in request.GET:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q)
-        return render_to_response('search_results.html',
-            {'books': books, 'query': q})
-    else:
-        return HttpResponse('Please submit a search term.')
+        if not q:
+            errors.append('Enter a search term.')
+        elif len(q)>20:
+            errors.append('Please enter at most 20 characters.')
+        else:
+            books = Book.objects.filter(title__icontains=q)
+            return render_to_response('search_results.html',
+                {'books': books, 'query': q})
+    #else:
+        #return HttpResponse('Please submit a search term.')
+    return render_to_response('search_form.html',{'errors': errors})
 
